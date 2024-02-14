b27c5c4f4021ce39193a2fd614348b42c4261396
maktheus <maktheus@gmail.com>
2024-02-14 16:26:09 -0400

    UnifyVideo finish without audio

--- src/Video/UnifyVideos.py
+++ src/Video/UnifyVideos.py
@@ -1,4 +1,68 @@
 import cv2
+import os
+import time
+
+def crop_video(video_path):
+    # Abre o vídeo
+    cap = cv2.VideoCapture(video_path)
+
+    # Pega as propriedades do vídeo
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Determina se o vídeo está no formato paisagem ou retrato
+    is_landscape = width > height
+
+    # Define as dimensões do corte
+    if is_landscape:
+        new_width = height
+        new_height = height
+        start_x = int((width - new_width) / 2)
+        start_y = 0
+    else:
+        new_width = width
+        new_height = width
+        start_x = 0
+        start_y = int((height - new_height) / 2)
+
+    # Define o nome do arquivo para o vídeo cortado cropeed_video+timestamp
+    nameFile =  'cropped_video' + str(int(time.time())) + '.avi'
+    filep = 'video_files/' + nameFile
+
+    # Cria o objeto para escrever o vídeo de saída
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    #salve dentro de video_files
+    output_video = cv2.VideoWriter(filep, fourcc, 30, (new_width, new_height))
+
+    # Loop para processar cada frame
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        # Faz o corte no frame
+        cropped_frame = frame[start_y:start_y + new_height, start_x:start_x + new_width]
+
+        # Escreve o frame cortado no vídeo de saída
+        output_video.write(cropped_frame)
+
+    # Libera os recursos
+    cap.release()
+    output_video.release()
+    cv2.destroyAllWindows()
+
+    #retorne o caminho do vídeo cortado
+    return filep
+
+
+def cleanAllCroppedVideos():
+    for file in os.listdir("video_files"):
+        if file.startswith("cropped_video"):
+            os.remove(f"video_files/{file}")
+
+
 
 
 def UnifyVideos(FirstVideo, SecondVideo):
@@ -36,18 +100,17 @@ def UnifyVideos(FirstVideo, SecondVideo):
         # Escrever o frame empilhado no vídeo de saída
         output_video.write(stacked_frame)
 
-        # Mostrar o frame na tela (opcional)
-        cv2.imshow('Stacked Video', stacked_frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
     # Libera os recursos
     cap1.release()
     cap2.release()
     output_video.release()
     cv2.destroyAllWindows()
 
+    cleanAllCroppedVideos()
+
     return 'output_video.avi'
 
-UnifyVideos('video_files/the_strange_and_dark_cartoon_network_iceberg.mp3_segmento_1.mp4',
-             'video_files/the_strange_and_dark_cartoon_network_iceberg.mp3_segmento_2.mp4')
+#UnifyVideos('video_files/the_strange_and_dark_cartoon_network_iceberg.mp3_segmento_1.mp4',
+#            'video_files/the_strange_and_dark_cartoon_network_iceberg.mp3_segmento_2.mp4')
+UnifyVideos(crop_video('video_files/the_strange_and_dark_cartoon_network_iceberg.mp3_segmento_1.mp4'),
+            crop_video('video_files/the_strange_and_dark_cartoon_network_iceberg.mp3_segmento_2.mp4'))
